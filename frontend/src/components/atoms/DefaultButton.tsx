dd32d98d06bdecff9f7c5c69b3e593dea68e2195
saku-1101 <sakupi1101@outlook.jp>
2023-03-22 15:19:41 +1100

    [fix]follows daisy ui style

--- frontend/src/components/atoms/DefaultButton.tsx
+++ frontend/src/components/atoms/DefaultButton.tsx
@@ -1,15 +1,9 @@
 import { Theme, Button } from 'react-daisyui';
 
-export default function DefaultButton() {
+export default function DefaultButton(props: { label: string }) {
   return (
     <>
-      <Theme dataTheme="dark">
-        <Button color="primary">Click me, dark!</Button>
-      </Theme>
-      <Theme dataTheme="light">
-        <Button color="primary">Click me, light!</Button>
-      </Theme>
-      obsidian
+      <button className="btn btn-primary">{props.label}</button>
     </>
   );
 }
