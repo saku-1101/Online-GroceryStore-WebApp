75ad734ea4cf35740b0d510bdc9431f965127df2
saku-1101 <sakupi1101@outlook.jp>
2023-03-23 16:45:11 +1100

    [refactor]nav bar内のコンポーネント化

---
+++ frontend/src/components/atoms/SearchBox.tsx
@@ -0,0 +1,9 @@
+export default function SearchBox() {
+  return (
+    <>
+      <div className="form-control">
+        <input type="text" placeholder="Search" className="input input-bordered" />
+      </div>
+    </>
+  );
+}
