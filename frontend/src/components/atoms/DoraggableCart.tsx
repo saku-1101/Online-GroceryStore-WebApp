27d6911453b7b67222c819a6638970dfbd65b87d
saku-1101 <sakupi1101@outlook.jp>
2023-03-23 16:36:25 +1100

    [fix]draggable button modified

--- frontend/src/components/atoms/DoraggableCart.tsx
+++ frontend/src/components/atoms/DoraggableCart.tsx
@@ -1,25 +1,22 @@
 import { useState } from 'react';
 import Draggable from 'react-draggable';
 
-export default function DoraggableCart() {
+export default function DraggableCart() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   const handleDrag = (e: any, ui: any) => {
-    const { x, y } = position;
+    const { x, y } = ui;
     setPosition({
-      x: x + ui.deltaX,
-      y: y + ui.deltaY,
+      x: x,
+      y: y,
     });
   };
 
   return (
-    <Draggable onDrag={handleDrag}>
-      <div
-        className="fixed right-0 bottom-0 mr-4 mb-4 p-4 bg-white border rounded-lg shadow-sm"
-        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
-      >
-        Cart
-      </div>
+    <Draggable onDrag={handleDrag} position={position}>
+      <button className="btn btn-lg no-animation btn-circle fixed right-0 bottom-0 mr-4 mb-4 bg-white border shadow-sm text-2xl z-20">
+        🛒
+      </button>
     </Draggable>
   );
 }
