6e2e944c16e194627201e426e1e8a756a0b01672
saku-1101 <sakupi1101@outlook.jp>
2023-03-24 20:21:37 +1100

    [fix]docker compose により生まれたエラーを解決：npm run devでは反応しないのにbuildで反応するエラーの解消

--- frontend/src/error-page.tsx
+++ frontend/src/error-page.tsx
@@ -1,16 +1,37 @@
-import { useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
 
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 401) {
+      // ...
+    } else if (error.status === 404) {
+      // ...
+    }
+
+    return (
+      <div id="error-page" className="w-screen">
+        <h1 className="text-center">Oops! {error.status}</h1>
+        <p className="text-center">{error.statusText}</p>
+        {error.data?.message && (
+          <p className="text-center">
+            <i>{error.data.message}</i>
+          </p>
+        )}
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div id="error-page" className="w-screen flex flex-col">
+        <h1 className="text-center">Oops! Unexpected Error</h1>
+        <p className="text-center">Something went wrong.</p>
+        <p className="text-center">
+          <i>{error.message}</i>
+        </p>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 }
