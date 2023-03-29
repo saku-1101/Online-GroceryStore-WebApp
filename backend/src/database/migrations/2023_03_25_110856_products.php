6f97b4e353fee403ce2f5deb1e9fd37c0b1c38b5
saku-1101 <sakupi1101@outlook.jp>
2023-03-29 18:32:20 +1100

    [fix]delete timespamp: Consider the case that I have to add the data manually

---
+++ backend/src/database/migrations/2023_03_25_110856_products.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * CAUTION: The table is already made in SQL.
+     * CAUTION: This is just an explicit definition.
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('product_name', 20);
+            $table->decimal('unit_price', 8, 2); # (M,D) means than values can be stored with up to M digits in total, of which D digits may be after the decimal point. 
+            $table->string('unit_quantity', 15);
+            $table->unsignedBigInteger('in_stock');
+            $table->unsignedBigInteger('category');
+            $table->string('url', 512);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
