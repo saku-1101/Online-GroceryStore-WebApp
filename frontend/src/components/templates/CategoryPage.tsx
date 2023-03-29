83abf212c2b04ca7557ee2d4eaa3f5de3add61c9
saku-1101 <sakupi1101@outlook.jp>
2023-03-29 20:44:52 +1100

    [add]category cards are displayed: connected via API

--- frontend/src/components/templates/CategoryPage.tsx
+++ frontend/src/components/templates/CategoryPage.tsx
@@ -1,19 +1,38 @@
+import { useEffect, useState } from 'react';
 import ProductCard from '../molecules/ProductCard';
 import PageTitle from '../atoms/PageTitle';
+import axios from 'axios';
 
 export default function CategoryPage() {
+  const [categories, setCategories] = useState([]);
+  const setData = async () => {
+    const res = await axios.get('http://localhost:8080/product/category');
+    console.log(res.data.products);
+    setCategories(res.data.products);
+  };
+  useEffect(() => {
+    setData();
+  }, []);
+
   return (
-    <div>
+    <div className="flex flex-col justify-center">
       <PageTitle title="Category" />
-      <ProductCard
-        imgUrl="./src/assets/bread.jpg"
-        imgAlt="veg"
-        category="Produces"
-        name=""
-        tag="inStock"
-        description="If a dog chews shoes whose shoes does he choose?"
-        button="Discover more!"
-      />
+      <div className="flex justify-center">
+        <div className="w-3/4 grid grid-cols-3 gap-10">
+          {categories.map((category) => (
+            <ProductCard
+              key={category.category}
+              imgUrl="src/assets/bread.jpg"
+              imgAlt={category.url}
+              category={category.category}
+              name={category.category}
+              tag=""
+              description=""
+              button="Discover more!"
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
