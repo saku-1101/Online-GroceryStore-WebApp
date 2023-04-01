a04c93f1c3c0144a3bf47555ebd2ba642d017397
saku-1101 <sakupi1101@outlook.jp>
2023-04-02 08:40:54 +1000

    [add]Product Interface

--- frontend/src/components/molecules/ProductCard.tsx
+++ frontend/src/components/molecules/ProductCard.tsx
@@ -1,37 +1,45 @@
+import { Product } from '../../core/models/Product.model';
 import DefaultButton from '../atoms/LinkToButton';
 import CountingButton from '../atoms/CountingButton';
 import OrderButton from '../atoms/OrderButton';
+import { useAppSelector } from '../../app/hooks';
+import { selectOrderId } from '../../features/counter/appSlice';
 
-export default function ProductCard(props: {
-  imgUrl: string;
-  imgAlt: string;
-  category: string;
-  name: string;
-  tag: string;
-  description: string;
-  button: string;
-  uri: string;
-  isCategory: boolean;
-}) {
+export default function ProductCard(props: { product: Product; button: string; isCategory: boolean }) {
+  const order_id = useAppSelector(selectOrderId);
+  // amountの定義が親に必要
   return (
     <>
       <div className="card card-compact w-full bg-base-100 shadow-xl p-0">
         <figure>
-          <img src={props.imgUrl} alt={props.imgAlt} />
+          <img src={props.product.url} alt="" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title text-neutral-focus">
-            {props.name}
-            <div className="badge badge-secondary">{props.tag}</div>
-          </h2>
-          <p className="text-neutral justify-start">{props.description}</p>
+          {props.isCategory ? (
+            <h2 className="card-title text-neutral-focus">{props.product.category}</h2>
+          ) : (
+            <h2 className="card-title text-neutral-focus">{props.product.product_name}</h2>
+          )}
+          {props.isCategory ? (
+            ''
+          ) : (
+            <div className="badge badge-secondary">{props.product.in_stock ? 'InStock' : 'OutOfStock'}</div>
+          )}
+          {props.isCategory ? '' : <p className="text-neutral justify-start">Product description comes here.</p>}
+          {/* <p className="text-neutral justify-start">{props.description}</p> */}
           <div className="card-actions justify-end">
             {props.isCategory ? (
-              <DefaultButton label={props.button} uri={props.uri} />
+              <DefaultButton label={props.button} uri={'/product/category/' + props.product.category} />
             ) : (
               <>
+                {/* (amount)親コンポーネントの値を子コンポーネントで変更してもらうようにしたいです */}
                 <CountingButton amount={0} />
-                <OrderButton label={props.button} order_id="34567" product_id="4" quantity="4" />
+                <OrderButton
+                  label={props.button}
+                  order_id={order_id}
+                  product_id={props.product.product_id}
+                  quantity={4}
+                />
               </>
             )}
           </div>
