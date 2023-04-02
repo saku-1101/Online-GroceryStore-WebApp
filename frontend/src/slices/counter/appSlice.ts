d8385e7a63d13a16b5d64727fbe9e9d253d445a8
saku-1101 <sakupi1101@outlook.jp>
2023-04-02 11:24:43 +1000

    [add]categories state will be watched grobally

--- frontend/src/slices/counter/appSlice.ts
+++ frontend/src/slices/counter/appSlice.ts
@@ -1,14 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from '../../app/store';
 
 // type for the slice state
 interface AppState {
   order_id: number;
+  categories: never[];
 }
 
 // initial state definition
 const initialState: AppState = {
   order_id: Math.floor(Math.random() * (1000000 - 0)),
+  categories: [],
 };
 
 // Slice is a combination of reducers with action
@@ -18,18 +20,17 @@ export const appSlice = createSlice({
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    // increment: (state) => {
-    //   state.order_id += 1;
-    // },
-    // decrement: (state) => {
-    //   state.amount -= 1;
-    // },
+    setCategory: (state, action: PayloadAction<never[]>) => {
+      state.categories = action.payload;
+      console.log(state.categories);
+    },
   },
 });
 
 // Export a part of actions of slice
-// export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const appActions = appSlice.actions;
 // Other code such as selectors can use the imported `RootState` type
 export const selectOrderId = (state: RootState) => state.app.order_id;
+export const selectCategory = (state: RootState) => state.app.categories;
 // Export a part of reducers of slice
 export const appReducer = appSlice.reducer;
