884bbce56094aa71f53696651835bfc07170d7cc
saku-1101 <sakupi1101@outlook.jp>
2023-04-02 11:27:25 +1000

    [refactor]

--- frontend/src/components/atoms/CountingButton.tsx
+++ frontend/src/components/atoms/CountingButton.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 export default function CountingButton(props: { amount: number; increaseAmount: any; decreaseAmount: any }) {
   // const [amount, setAmount] = useState(props.amount);
   return (
