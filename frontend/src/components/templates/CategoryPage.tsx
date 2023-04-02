a081a47f6d0c016a9ab6d220d4e377fd810c928e
saku-1101 <sakupi1101@outlook.jp>
2023-04-02 11:26:07 +1000

    [add]categories stateをここのロード時に更新

--- frontend/src/components/templates/CategoryPage.tsx
+++ frontend/src/components/templates/CategoryPage.tsx
@@ -3,16 +3,22 @@ import { useEffect, useState } from 'react';
 import ProductCard from '../molecules/ProductCard';
 import PageTitle from '../atoms/PageTitle';
 import { getCategories } from '../../core/infrastructures/AppApi';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { selectCategory, appActions } from '../../slices/counter/appSlice';
 
 export default function CategoryPage() {
-  const [categories, setCategories] = useState([]);
+  const dispatch = useAppDispatch();
+  const categories = useAppSelector(selectCategory);
+
   const setData = async () => {
     const res = await getCategories();
-    setCategories(res.products);
+    // store にcategoriesを登録
+    dispatch(appActions.setCategory(res.products));
+    console.log(res.products);
   };
   useEffect(() => {
     setData();
-  }, []);
+  }, [categories]);
 
   return (
     <div className="flex flex-col justify-center">
