482e539fca0c17577782fcfc34f86a2e02f5e201
saku-1101 <sakupi1101@outlook.jp>
2023-04-03 14:11:47 +1000

    [add]email template

---
+++ backend/src/resources/views/email/name.blade.php
@@ -0,0 +1,23 @@
+<div>
+    <p>Hey, {{$name}}!☺️ Thank you for shopping with us today!</p>
+    <p>Order ID: {{ $order->order_id  }}<p>
+    <p>Price: {{ $order->total_amount  }}<p>
+    <p>{{$order_detail}}</p>
+    <p>This order is placed at {{$when}} .</p>
+    <p>We're looking forward to your next visit!</p>
+    <p>
+Sincerely, <br>
+*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*<br>
+ 🇯🇵Japan<br>
+ <p></p>
+UTS ID: 24648418<br>
+<p></p>
+Name: Sakura Adachi<br>
+<p></p>
+Status: International Exchange student (Undergraduate)<br>
+<p></p>
+UTS Email Address: Sakura.Adachi@student.uts.edu.au<br>
+Available Email: 20238008@edu.cc.saga-u.ac.jp<br>
+<p></p>
+*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*</p>
+</div>
