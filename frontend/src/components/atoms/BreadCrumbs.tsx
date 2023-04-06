df114a3b4e29e78ffad86660a999897d46524e42
saku-1101 <sakupi1101@outlook.jp>
2023-04-06 13:00:04 +1000

    [add]sub category router add to bread crumbs

--- frontend/src/components/atoms/BreadCrumbs.tsx
+++ frontend/src/components/atoms/BreadCrumbs.tsx
@@ -1,10 +1,11 @@
 import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
-import { selectCategoryId } from '../../slices/appSlice';
+import { selectCategoryId, selectSubCategoryId } from '../../slices/appSlice';
 export default function BreadCrumbs() {
   const location = useLocation();
   const selected_category: number = useAppSelector(selectCategoryId);
+  const selected_sub_category: number = useAppSelector(selectSubCategoryId);
 
   return (
     <>
@@ -25,9 +26,21 @@ export default function BreadCrumbs() {
           </li>
           <li>
             <Link
-              to={'/product/category/' + selected_category}
+              to={'/category/' + selected_category}
               className={
-                location.pathname.startsWith('/product/category/') ? 'breadcrumb-active' : 'breadcrumb-not-active'
+                location.pathname === '/category/' + selected_category ? 'breadcrumb-active' : 'breadcrumb-not-active'
+              }
+            >
+              Sub Categories
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={'/category/' + selected_category + '/sub_category/' + selected_sub_category}
+              className={
+                location.pathname.startsWith('/category/' + selected_category + '/sub_category/')
+                  ? 'breadcrumb-active'
+                  : 'breadcrumb-not-active'
               }
             >
               Products
