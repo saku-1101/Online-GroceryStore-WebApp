721503ed3a13c0f807eeb366e121875617893398
saku-1101 <sakupi1101@outlook.jp>
2023-04-06 12:49:05 +1000

    [feat]アプリロードとともにcategoriesをstoreにセットし，categoriesの変更検知後，sub_categoryをsotreにセット

--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -3,21 +3,39 @@ import { Outlet } from 'react-router-dom';
 import NavBar from '../components/molecules/NavBar';
 import DraggableCart from '../components/atoms/DraggableCart';
 import Footer from '../components/atoms/Footer';
-import { getCategories } from '../core/infrastructures/AppApi';
+import { getCategories, getSubCategoriesByCategory } from '../core/infrastructures/AppApi';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
-import { selectCategory, appActions } from '../slices/appSlice';
+import { selectCategory, selectSubCategory, appActions } from '../slices/appSlice';
+import { Product } from '../core/models/Product.model';
 
 export default function Root() {
   const dispatch = useAppDispatch();
   const categories = useAppSelector(selectCategory);
+  const sub_categories = useAppSelector(selectSubCategory);
 
-  const setData = async () => {
+  const setCategory = async () => {
     const res = await getCategories();
     // store にcategoriesを登録
     dispatch(appActions.setCategory(res.products));
   };
+
+  const setSubCategory = async () => {
+    const res: Array<Product> = [];
+    for (const category of categories) {
+      const subcategory_list = await getSubCategoriesByCategory(category.category.toString());
+      subcategory_list.products.forEach((subcategory: Product) => {
+        res.push(subcategory);
+      });
+    }
+    // store にcategoriesを登録
+    dispatch(appActions.setSubCategory(res));
+    console.log(sub_categories);
+  };
   useEffect(() => {
-    setData();
+    setCategory();
+  }, []);
+  useEffect(() => {
+    setSubCategory();
   }, [categories]);
   return (
     <div className="flex flex-col justify-between h-screen">
