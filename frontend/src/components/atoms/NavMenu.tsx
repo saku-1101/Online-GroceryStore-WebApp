461d5da0955a0ae38529f74f0459d5e0c0c3031a
saku-1101 <sakupi1101@outlook.jp>
2023-04-06 13:06:45 +1000

    [add]カテゴリ＞サブカテゴリのプルダウンを表示

--- frontend/src/components/atoms/NavMenu.tsx
+++ frontend/src/components/atoms/NavMenu.tsx
@@ -1,9 +1,31 @@
 import { useAppSelector } from '../../app/hooks';
-import { selectCategory } from '../../slices/appSlice';
+import { selectCategory, selectSubCategory } from '../../slices/appSlice';
 import { Product } from '../../core/models/Product.model';
 import { Link } from 'react-router-dom';
+import { category_name, sub_category_name } from '../../core/data/replace.data';
+
 export default function NavMenu() {
   const categories = useAppSelector(selectCategory);
+  const sub_categories = useAppSelector(selectSubCategory);
+
+  const handleSubCategoryContent = (category: number) => {
+    const content = [];
+    for (const sub_category of sub_categories) {
+      if (sub_category.category == category) {
+        content.push(
+          <Link
+            to={'/category/' + category + '/sub_category/' + sub_category.sub_category}
+            key={sub_category.sub_category}
+          >
+            <li tabIndex={0} className="py-3">
+              {sub_category_name[sub_category.sub_category]}
+            </li>
+          </Link>,
+        );
+      }
+    }
+    return content;
+  };
   return (
     <>
       <div className="dropdown">
@@ -18,11 +40,12 @@ export default function NavMenu() {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
           </svg>
         </label>
-        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <ul tabIndex={0} className="menu dropdown-content mt-3 p-4 shadow bg-base-100 rounded-box">
           {categories.map((category: Product) => (
-            <Link to={'/product/category/' + category.category} key={category.category}>
-              <li>{category.category}</li>
-            </Link>
+            <li key={category.category}>
+              <span>{category_name[category.category]}</span>
+              <ul className="rounded-box p-4 bg-base-100">{handleSubCategoryContent(category.category)}</ul>
+            </li>
           ))}
         </ul>
       </div>
