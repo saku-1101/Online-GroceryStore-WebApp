ba745ba51245630e9b247e97bf2084f83e11e560
saku-1101 <sakupi1101@outlook.jp>
2023-04-06 12:57:51 +1000

    [add]router for sub category page

--- frontend/src/components/templates/SelectTheCategoryError.tsx
+++ frontend/src/components/templates/SelectTheCategoryError.tsx
@@ -2,8 +2,7 @@ import { Link } from 'react-router-dom';
 export default function SelectTheCategoryError() {
   return (
     <h1 className="md:my-10 lg:my-20 text-2xl text-center">
-      {' '}
-      Select the category in <Link to="/category">Category</Link>, then come back!
+      Select the Category and Sub category from <Link to="/category">Category</Link>, then come back!
     </h1>
   );
 }
